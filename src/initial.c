81a4a07bfda6d71defcd31f46b1b31f33c69f49a
dianmo42 <dianmo42@outlook.com>
2024-12-02 17:02:36 +0800

    feat: add new sampling method, nevery = -1 and 0

--- src/initial.c
+++ src/initial.c
@@ -32,13 +32,13 @@ void Initial()
     // check parameters
     if (nfreq < 1)
         ErrorExit("Error: nfreq must be positive\n");
-    if (nevery < 0)
-        ErrorExit("Error: nevery must be 0 or positive\n");
+    if (nevery < -1)
+        ErrorExit("Error: nevery must be -1, 0 or positive\n");
     if (nrepeat < 1)
         ErrorExit("Error: nrepeat must be positive\n");
     if (nframe - nevery * nrepeat < 0)
         ErrorExit("Error: nframe must be greater than nevery * nrepeat\n");
-    if (nevery == 0)
+    if (nevery == -1 || nevery == 0)
         nref = (nframe - nrepeat) / nfreq;
     else
         nref = (nframe - nevery * nrepeat) / nfreq;
@@ -53,13 +53,27 @@ void Initial()
     // when nevery = 0, set correlation time on logarithmic scale
     // otherwise set correlation time on linear scale
     t_corr = (int *)malloc(nrepeat * sizeof(int));
-    if (nevery == 0)
+    if (nevery == -1)
     {
         t_corr[0] = 1;
         int i = 1;
         while (1)
         {
             t_corr[i] = t_corr[i - 1] + pow(10, (int)((i - 1) / 9));
+            if (t_corr[i] > nrepeat)
+                break;
+            ++i;
+        }
+        nrepeat = i;
+    }
+    else if (nevery == 0)
+    {
+        int i;
+        for (i = 0; i < 100; ++i)
+            t_corr[i] = i + 1;
+        while (1)
+        {
+            t_corr[i] = t_corr[i - 1] + pow(10, (int)(i - 100) / 90 + 1);
             if (t_corr[i] > nrepeat)
                 break;
             ++i;
@@ -88,6 +102,11 @@ void Initial()
         overlap = (real *)malloc(nrepeat * sizeof(real));
         chi4_Q = (real *)malloc(nrepeat * sizeof(real));
     }
+
+    #ifdef DEBUG
+        for (int i = 0; i < nrepeat; ++i)
+            fprintf(stdout, "%d  %d\n", i, t_corr[i]);
+    #endif
 
     return;
 }
